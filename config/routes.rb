9cdea74d30d24f4b390fe69a41d5750869804e02
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-06-01 15:58:58 +0000

    [Update]4-27(4-11まで)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+  get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
