5eea9c1757b4b1b8331f41724de90f13c3123d32
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-06-03 17:14:10 +0000

    [Update]4-27(4-16エラー)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def index
@@ -12,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params_update
-      params.require(:user).permit(:name, :email, :image, :introduction)
+    def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
     end
 end
