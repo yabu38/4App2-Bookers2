8c1dd274dcddf924f70d7dee80f945b2afeb4e65
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-06-07 05:54:59 +0000

    [Update]4-27(users#show作成途中)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,26 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(@user.id)
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
     root_path
   end
 
+  layout :layout_by_resource
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+  
+  def layout_by_resource
+    if devise_controller?
+      "sub-layout"
+    else
+      "application"
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
